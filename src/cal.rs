fc03d0daa627e6bbabdef5a456f961165c9b8d72
Parker Moore <parkrmoore@gmail.com>
2015-01-17 01:08:10 -0800

    PROGRESS.

--- src/cal.rs
+++ src/cal.rs
@@ -1,10 +1,7 @@
 extern crate time;
 use time::*;
 
-fn tm_to_string(time: Tm) -> Str {
-  return format!("BBB {}", time);
-}
-
 fn main() {
-  println!("This is my time: {}", tm_to_string(now()));
+    let time = now();
+    println!("This is my time: {}", time.rfc3339());
 }
