768f31edcd64475d3a94cc6ebab3130a20788fcf
Parker Moore <parkrmoore@gmail.com>
2015-05-18 17:16:22 +0700

    Update for Rust 1.0

--- src/cal.rs
+++ src/cal.rs
@@ -1,26 +1,24 @@
 extern crate time;
-use std::time::duration::Duration;
 
 fn main() {
     let now = time::now_utc();
-    let time_since_first = Duration::days((now.tm_mday - 1) as i64);
+    let time_since_first = time::Duration::days((now.tm_mday - 1) as i64);
     let seconds_of_first_day = now.to_timespec() - time_since_first;
     let first_of_the_month = time::at_utc(seconds_of_first_day);
+    let one_day = time::Duration::hours(24);
 
-    print!("{:>11}", now.strftime("%B").ok().unwrap());
-    println!(" {:<8}", now.strftime("%Y").ok().unwrap());
+    print!("{:>11} {:<8}\n",
+        now.strftime("%B").ok().unwrap().to_string(),
+        now.strftime("%Y").ok().unwrap().to_string()
+    );
     println!("Su Mo Tu We Th Fr Sa");
-
-    let one_day = Duration::hours(24);
 
     let this_month = now.tm_mon;
     let mut current_day = first_of_the_month;
 
     if current_day.tm_wday > 0 {
-        let mut count = 0u32;
-        while (count as i32) < current_day.tm_wday {
+        for _ in 0..current_day.tm_wday {
             print!("   ");
-            count += 1;
         }
     }
     while current_day.tm_mon == this_month {
